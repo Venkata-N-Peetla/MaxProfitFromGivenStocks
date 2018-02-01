7a8fd59dd5120f563bb1178258848b6f89e19810
Venkata Narasimham Peetla <venkata.n.peetla@kp.org>
2018-01-31 22:07:21 -0800

    Implemented the function which returns from an array of given stocks. We are using the Greedy algorithm for this method.

--- MaxProfitFromGivenStocks/ViewController.m
+++ MaxProfitFromGivenStocks/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSNumber *profit = GetMaximumProfitFromGivenStocksArray(@[@10, @30, @5, @35, @40], 6);
+    NSLog(@"Profit value = %ld", profit.integerValue);
 }
 
 
@@ -25,5 +28,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+//Greedy algorithm:
+NSNumber *GetMaximumProfitFromGivenStocksArray(NSArray<NSNumber *> *stocksInputArray, NSUInteger lenghth) {
+
+    NSInteger maxProfit;
+    NSInteger minPrice;
+
+    NSCAssert(lenghth > 2, @"We need minimum two get max profit");
+
+    minPrice = stocksInputArray[0].integerValue;
+    maxProfit = stocksInputArray[1].integerValue - stocksInputArray[0].integerValue;
+
+    for (NSUInteger i = 1; i < lenghth-1; i++) {
+        NSInteger currentPrice = stocksInputArray[i].integerValue;
+        NSInteger potentialProfit = currentPrice - minPrice;
+        maxProfit = MAX(maxProfit, potentialProfit);
+        minPrice = MIN(minPrice, currentPrice);
+    }
+
+    return @(maxProfit);
+
+}
 
 @end
